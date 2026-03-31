cf5d497c688f71372973546926f2ee6ce5037786
mohamednoaman17819-arch <mohamed.noaman17819@gmail.com>
2026-03-31 21:50:58 +0400

    Add files via upload

---
+++ lec_4.py
@@ -0,0 +1,35 @@
+# Comparsion Operators 
+#> < == !=
+# print(5>3)
+#print(5<3)
+# print(7 == 7) #a=5
+# print(7 != 7)
+
+#bitwise operators
+# 5 0101
+# 3 0011
+# flase 0
+# True 1
+# print(int(False))
+
+# And &
+#print(5 & 3) #   0001
+
+#or |
+#print(5|3) #  0111
+
+#xor ^ 
+# 5 0101
+# 3 0011
+#print(5^3) #    0110
+
+#not ~
+#print(~True) #00000001010  10  ~x = -(x+1)
+
+# <<
+# 0101   1010
+print(5<<2)  # x<< n = x * (2^n)   5*4=20
+
+#>> 
+#0101   0010
+print(5>>1)  # x >> n = floor( x / (2^n ))
